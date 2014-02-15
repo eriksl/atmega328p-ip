88604dbb6fd2ecd775f0d4aad78ccd9ede9210ad
Erik Slagter <erik@slagter.name>
2014-02-15 12:24:20 +0100

    Stats
    - move text stats generation into stats.c
    - change size from 16 to 8 bits, saves ram and output

--- stats.c
+++ stats.c
@@ -1,22 +1,115 @@
 #include "stats.h"
+#include "util.h"
 
-uint16_t eth_interrupts = 0;
-uint16_t eth_pkt_rx = 0;
-uint16_t eth_pkt_tx = 0;
-uint16_t eth_txerr = 0;
-uint16_t eth_rxerr = 0;
-uint16_t eth_pkts_buffered = 0;
+stats_t eth_interrupts = 0;
+stats_t eth_pkt_rx = 0;
+stats_t eth_pkt_tx = 0;
+stats_t eth_txerr = 0;
+stats_t eth_rxerr = 0;
+stats_t eth_pkts_buffered = 0;
 
-uint16_t ip_arp_pkt_in = 0;
-uint16_t ip_arp_pkt_out = 0;
-uint16_t ip_ipv4_pkt_in = 0;
-uint16_t ip_ipv4_pkt_out = 0;
-uint16_t ip_icmp4_pkt_in = 0;
-uint16_t ip_icmp4_pkt_out = 0;
-uint16_t ip_tcp4_pkt_in = 0;
-uint16_t ip_tcp4_pkt_out = 0;
-uint16_t ip_other_pkt = 0;
-uint16_t ip_stray_pkt = 0;
-uint16_t ip_bad_checksum = 0;
+stats_t ip_arp_pkt_in = 0;
+stats_t ip_arp_pkt_out = 0;
+stats_t ip_ipv4_pkt_in = 0;
+stats_t ip_ipv4_pkt_out = 0;
+stats_t ip_icmp4_pkt_in = 0;
+stats_t ip_icmp4_pkt_out = 0;
+stats_t ip_tcp4_pkt_in = 0;
+stats_t ip_tcp4_pkt_out = 0;
+stats_t ip_udp4_pkt_in = 0;
+stats_t ip_udp4_pkt_out = 0;
+stats_t ip_other_pkt = 0;
+stats_t ip_stray_pkt = 0;
+stats_t ip_bad_checksum = 0;
 
-uint16_t wd_interrupts = 0;
+stats_t wd_interrupts = 0;
+
+void stats_generate(uint16_t size, uint8_t *dst)
+{
+	static uint8_t conv[8];
+
+	xstrncat((uint8_t *)"wd int: ", size, dst);
+	int_to_str(wd_interrupts, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"e int: ", size, dst);
+	int_to_str(eth_interrupts, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"e rx: ", size, dst);
+	int_to_str(eth_pkt_rx, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"e tx: ", size, dst);
+	int_to_str(eth_pkt_tx, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"e rxerr: ", size, dst);
+	int_to_str(eth_rxerr, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"e txerr: ", size, dst);
+	int_to_str(eth_txerr, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"e rxbuf: ", size, dst);
+	int_to_str(eth_pkts_buffered, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"arp: ", size, dst);
+	int_to_str(ip_arp_pkt_in, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"/", size, dst);
+	int_to_str(ip_arp_pkt_out, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"ipv4: ", size, dst);
+	int_to_str(ip_ipv4_pkt_in, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"/", size, dst);
+	int_to_str(ip_ipv4_pkt_out, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"icmp4: ", size, dst);
+	int_to_str(ip_icmp4_pkt_in, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"/", size, dst);
+	int_to_str(ip_icmp4_pkt_out, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"udp4: ", size, dst);
+	int_to_str(ip_udp4_pkt_in, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"/", size, dst);
+	int_to_str(ip_udp4_pkt_out, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"tcp4: ", size, dst);
+	int_to_str(ip_tcp4_pkt_in, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"/", size, dst);
+	int_to_str(ip_tcp4_pkt_out, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"other: ", size, dst);
+	int_to_str(ip_other_pkt, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+
+	xstrncat((uint8_t *)"stray: ", size, dst);
+	int_to_str(ip_stray_pkt, sizeof(conv), conv);
+	xstrncat(conv, size, dst);
+	xstrncat((uint8_t *)"\n", size, dst);
+}
--- stats.h
+++ stats.h
@@ -3,25 +3,31 @@
 
 #include <stdint.h>
 
-extern uint16_t eth_interrupts;
-extern uint16_t eth_pkt_rx;
-extern uint16_t eth_pkt_tx;
-extern uint16_t eth_txerr;
-extern uint16_t eth_rxerr;
-extern uint16_t eth_pkts_buffered;
+typedef uint8_t stats_t;
 
-extern uint16_t ip_arp_pkt_in;
-extern uint16_t ip_arp_pkt_out;
-extern uint16_t ip_ipv4_pkt_in;
-extern uint16_t ip_ipv4_pkt_out;
-extern uint16_t ip_icmp4_pkt_in;
-extern uint16_t ip_icmp4_pkt_out;
-extern uint16_t ip_tcp4_pkt_in;
-extern uint16_t ip_tcp4_pkt_out;
-extern uint16_t ip_other_pkt;
-extern uint16_t ip_stray_pkt;
-extern uint16_t ip_bad_checksum;
+extern stats_t eth_interrupts;
+extern stats_t eth_pkt_rx;
+extern stats_t eth_pkt_tx;
+extern stats_t eth_txerr;
+extern stats_t eth_rxerr;
+extern stats_t eth_pkts_buffered;
 
-extern uint16_t wd_interrupts;
+extern stats_t ip_arp_pkt_in;
+extern stats_t ip_arp_pkt_out;
+extern stats_t ip_ipv4_pkt_in;
+extern stats_t ip_ipv4_pkt_out;
+extern stats_t ip_icmp4_pkt_in;
+extern stats_t ip_icmp4_pkt_out;
+extern stats_t ip_tcp4_pkt_in;
+extern stats_t ip_tcp4_pkt_out;
+extern stats_t ip_udp4_pkt_in;
+extern stats_t ip_udp4_pkt_out;
+extern stats_t ip_other_pkt;
+extern stats_t ip_stray_pkt;
+extern stats_t ip_bad_checksum;
+
+extern stats_t wd_interrupts;
+
+void stats_generate(uint16_t size, uint8_t *dst);
 
 #endif

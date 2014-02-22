7148dd866cf1962740c9f88e47606b83a6bb5285
Erik Slagter <erik@slagter.name>
2014-02-22 19:32:50 +0100

    Forgot source files in previous commit :-/

---
+++ twi_master.c
@@ -0,0 +1,181 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "twi_master.h"
+#include "watchdog.h"
+
+static inline uint8_t status(void)
+{
+	static uint8_t twsr;
+
+	twsr = TWSR & 0xf8;
+	return(twsr >> 3);
+}
+
+static inline void wait(void)
+{
+	while(!(TWCR & _BV(TWINT)))
+		(void)0;
+}
+
+static uint8_t send_start(void)
+{
+	static uint8_t stat;
+
+	TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWSTA);
+
+	wait();
+	stat = status();
+
+	if(stat != tms_start_sent)
+		return(tme_send_start | (stat << 4));
+
+	return(tme_ok);
+}
+
+static uint8_t send_address(uint8_t address, uint8_t request_write)
+{
+	static uint8_t stat;
+
+	TWDR = (address << 1) | (request_write ? 0 : 1);
+	TWCR = _BV(TWINT) | _BV(TWEN);
+
+	wait();
+	stat = status();
+
+	if(request_write)
+	{
+		if(stat != tms_addr_w_ack)
+			return(tme_send_address_w | (stat << 4));
+	}
+	else
+	{
+		if(stat != tms_addr_r_ack)
+			return(tme_send_address_r | (stat << 4));
+	}
+
+	return(tme_ok);
+}
+
+static uint8_t send_byte(uint8_t data)
+{
+	static uint8_t stat;
+
+	TWDR = data;
+	TWCR = _BV(TWINT) | _BV(TWEN);
+
+	wait();
+	stat = status();
+
+	if(stat != tms_data_sent_ack)
+		return(tme_send_byte | (stat << 4));
+
+	return(tme_ok);
+}
+
+static uint8_t receive_byte(uint8_t *data)
+{
+	static uint8_t stat;
+
+	TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN);
+
+	wait();
+	stat = status();
+
+	if(stat != tms_data_recvd_ack)
+		return(tme_receive_byte | (stat << 4));
+
+	*data = TWDR;
+
+	return(tme_ok);
+}
+
+static void send_stop(void)
+{
+	TWCR = _BV(TWINT) | _BV(TWSTO) | _BV(TWEN);
+
+	while(TWCR & _BV(TWSTO))
+		(void)0;
+}
+
+void twi_master_init(void)
+{
+	PRR		|= _BV(PRTWI);
+	TWCR	= 0x00;
+	DDRC	&= ~(_BV(5) | _BV(6));	// SCL, SDA HiZ
+
+	PRR		&= ~_BV(PRTWI);
+	TWBR	= 32;
+	TWSR	= 0;
+	TWDR	= 0xff;
+	TWCR	= _BV(TWEN);
+}
+
+void twi_master_recover(void)
+{
+	static uint8_t ix;
+
+	// try to send stop condition
+
+	TWCR = _BV(TWINT) | _BV(TWSTO) | _BV(TWEN);
+	_delay_ms(1);
+	TWCR = _BV(TWINT) | _BV(TWEN);
+	_delay_ms(1);
+
+	// try to resolve bus locked by slave
+
+	TWCR = 0x00;	// disable TWI, make ports available
+
+	DDRC |= _BV(5);		//	SCL	-> output
+	DDRC &= ~_BV(6);	//	SDA	-> HiZ
+
+	for(ix = 16; ix > 0; --ix)
+	{
+		PORTC &= ~_BV(5);	// SCL low
+		_delay_ms(1);
+		PORTC |= _BV(5);	// SCL high
+		_delay_ms(1);
+	}
+
+	return(twi_master_init());
+}
+
+uint8_t twi_master_send(uint8_t address, uint8_t length, const uint8_t *buffer)
+{
+	static uint8_t rv;
+
+	if((rv = send_start()) != tme_ok)
+		return(rv);
+
+	if((rv = send_address(address, 1)) != tme_ok)
+		return(rv);
+
+	for(; length > 0; length--, buffer++)
+		if((rv = send_byte(*buffer)) != tme_ok)
+			return(rv);
+
+	send_stop();
+
+	return(tme_ok);
+}
+
+uint8_t twi_master_receive(uint8_t address, uint8_t size, uint8_t *buffer)
+{
+	static uint8_t rv, ix;
+
+	if((rv = send_start()) != tme_ok)
+		return(rv);
+
+	if((rv = send_address(address, 0)) != tme_ok)
+		return(rv);
+
+	for(ix = 0; ix < size; ix++)
+	{
+		if((rv = receive_byte(&buffer[ix])) != tme_ok)
+			return(rv);
+	}
+
+	send_stop();
+
+	return(tme_ok);
+}
---
+++ twi_master.h
@@ -0,0 +1,36 @@
+#ifndef _twi_master_h_
+#define _twi_master_h_
+
+#include <stdint.h>
+
+typedef enum
+{
+	tms_start_sent		= 0x01,		//	0x08
+	tms_re_start_sent	= 0x02,		//	0x10
+	tms_addr_w_ack		= 0x03,		//	0x18
+	tms_addr_w_nack		= 0x04,		//	0x20
+	tms_data_sent_ack	= 0x05,		//	0x28
+	tms_data_send_nack	= 0x06,		//	0x30
+	tms_arbitration_lost= 0x07,		//	0x38
+	tms_addr_r_ack		= 0x08,		//	0x40
+	tms_addr_r_nack		= 0x09,		//	0x48
+	tms_data_recvd_ack	= 0x0a,		//	0x50
+	tms_data_recvd_nack	= 0x0b,		//	0x58
+} twi_master_state;
+
+typedef enum
+{
+	tme_ok				= 0x00,
+	tme_send_start 		= 0x01,
+	tme_send_address_w	= 0x02,
+	tme_send_address_r	= 0x03,
+	tme_send_byte		= 0x04,
+	tme_receive_byte	= 0x05,
+} twi_master_error_t;
+
+void	twi_master_init(void);
+void	twi_master_recover(void);
+uint8_t	twi_master_send(uint8_t address, uint8_t length, const uint8_t *buffer);
+uint8_t twi_master_receive(uint8_t address, uint8_t size, uint8_t *buffer);
+
+#endif

7a5f8e75a49ddb588b1ed9541666895c5bd758f3
Erik Slagter <erik@slagter.name>
2014-08-16 16:53:09 +0200

    cleanup no longer used int_to_hex and hex_to_int functions.

--- util.h
+++ util.h
@@ -1,13 +1,10 @@
-#ifndef _util_h_
-#define _util_h_
+#ifndef util_h
+#define util_h
 
 #include <stdint.h>
 
 void reset(void);
 void sleep(uint16_t ms);
 void pause(void);
-
-uint8_t hex_to_int(uint16_t *length, uint8_t const **in, uint8_t *value);
-void int_to_hex(uint8_t *buffer, uint8_t value);
 
 #endif

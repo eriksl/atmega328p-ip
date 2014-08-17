fdd4b9fd303381d2940d73a95658583ee2cdb04f
Erik Slagter <erik@slagter.name>
2014-08-17 12:51:48 +0200

    Preliminary temperature reading support (tmp36 on adc0).

--- application.c
+++ application.c
@@ -36,6 +36,7 @@ static uint8_t application_function_quit(uint8_t nargs, uint8_t args[num_args][l
 static uint8_t application_function_reset(uint8_t nargs, uint8_t args[num_args][length_args], uint16_t size, uint8_t *dst);
 static uint8_t application_function_stack(uint8_t nargs, uint8_t args[num_args][length_args], uint16_t size, uint8_t *dst);
 static uint8_t application_function_stats(uint8_t nargs, uint8_t args[num_args][length_args], uint16_t size, uint8_t *dst);
+static uint8_t application_function_temp_read(uint8_t nargs, uint8_t args[num_args][length_args], uint16_t size, uint8_t *dst);
 static uint8_t application_function_twiaddress(uint8_t nargs, uint8_t args[num_args][length_args], uint16_t size, uint8_t *dst);
 static uint8_t application_function_twiread(uint8_t nargs, uint8_t args[num_args][length_args], uint16_t size, uint8_t *dst);
 static uint8_t application_function_twireset(uint8_t nargs, uint8_t args[num_args][length_args], uint16_t size, uint8_t *dst);
@@ -47,6 +48,7 @@ static const __flash char description_quit[] = "quit";
 static const __flash char description_reset[] = "reset system";
 static const __flash char description_stack[] = "free memory";
 static const __flash char description_stats[] = "statistics";
+static const __flash char description_temp_read[] = "read temp sensor";
 static const __flash char description_twiaddress[] = "set TWI slave";
 static const __flash char description_twiread[] = "read n bytes from TWI";
 static const __flash char description_twireset[] = "reset TWI";
@@ -115,6 +117,12 @@ static const __flash application_function_table_t application_function_table[] =
 		0,
 	},
 	{
+		"tempr",
+		2,
+		application_function_temp_read,
+		description_temp_read,
+	},
+	{
 		"twia",
 		1,
 		application_function_twiaddress,
@@ -148,8 +156,14 @@ static const __flash application_function_table_t application_function_table[] =
 
 uint8_t application_init(void)
 {
+	/* timer1 for pwm */
+
 	timer1_init_pwm1a1b(timer1_1);	// pwm timer 1 resolution: 16 bits, frequency = 122 Hz
 	timer1_start();
+
+	/* adc */
+
+	DIDR0 = _BV(ADC0D);
 
 	return(WATCHDOG_PRESCALER_8192);
 }
@@ -321,6 +335,96 @@ static uint8_t application_function_stack(uint8_t nargs, uint8_t args[num_args][
 	static const __flash char stackfree_fmt[] = "Stackmonitor: %d bytes free\n";
 
 	snprintf_P((char *)dst, (size_t)size, stackfree_fmt, stackmonitor_free());
+
+	return(1);
+}
+
+static uint16_t get_adc(void)
+{
+	uint16_t rv;
+
+	ADCSRA |= _BV(ADSC);
+
+	while(ADCSRA & _BV(ADSC))
+		(void)0;
+
+	rv  = ADCL;
+	rv |= ADCH << 8;
+
+	return(rv);
+}
+
+static uint8_t application_function_temp_read(uint8_t nargs, uint8_t args[num_args][length_args], uint16_t size, uint8_t *dst)
+{
+	// ADC:		0 V = 0 pcm, 1.1 V = 1023 pcm
+	// 			mV = pcm / 1023 * 1100
+	// tmp36:	250 mV = -25.0 C, 500 mV = 0.0, 750 mV = 25.0 C
+	// 			C = (mV - 500) / 10
+
+	static const __flash char ok[] = "> temp sensor %d ok value %.1f\n";
+	static const __flash char error[] = "> invalid sensor\n";
+	uint8_t sensor;
+	uint8_t ix;
+	uint32_t total;
+	float calibrated;
+
+	sensor = (uint8_t)strtoul((const char *)args[1], 0, 0);
+
+	switch(sensor)
+	{
+		case(0):
+		{
+			PRR &= ~_BV(PRADC);
+			ADCSRB = 0x00;
+			break;
+		}
+	}
+
+	switch(sensor)
+	{
+		case(0):
+		{
+			ADMUX =		_BV(REFS1) | _BV(REFS0);	// ref = 1.1V, input = ADC0
+			ADCSRA =	_BV(ADEN)  | _BV(ADIF);
+						_BV(ADPS2) | _BV(ADPS1);	// 8 Mhz / 64 = 125 kHz > 50 < 200 kHz
+			ADCSRB =	0x00;
+
+			//calibrated = ((float)get_adc() * 1080.0) / 1024.0;
+			//calibrated = (calibrated - 500) / 10;
+
+			for(ix = 8; ix > 0; ix--)
+				get_adc();
+
+			total = 0;
+
+			for(ix = 255; ix > 0; ix--)
+				total += get_adc();
+
+			calibrated = total / 256;
+
+			snprintf_P((char *)dst, size, ok, sensor, calibrated);
+			break;
+		}
+
+		default:
+		{
+			strlcpy_P((char *)dst, error, (size_t)size);
+			break;
+		}
+	}
+
+	switch(sensor)
+	{
+		case(0):
+		{
+			ADMUX	 = 0x00;
+			ADCSRA	 = 0x00;
+			ADCSRB	 = 0x00;
+			PRR		|= _BV(PRADC);
+
+			break;
+		}
+	}
 
 	return(1);
 }

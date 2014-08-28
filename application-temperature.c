c59c9d3512e89a3dd7634c432d9bd417d93025bd
Erik Slagter <erik@slagter.name>
2014-08-28 09:42:58 +0200

    application-temperature: shuffle bandgap sensor and tmp36 around, so bg sensors comes first, as it's always present.

--- application-temperature.c
+++ application-temperature.c
@@ -78,16 +78,16 @@ uint8_t application_function_temp_read(uint8_t nargs, uint8_t args[application_n
 
 	switch(sensor)
 	{
-		case(0): // TMP36
-		case(1): // internal bandgap thermosensor
+		case(0): // internal bandgap thermosensor
+		case(1): // TMP36
 		{
 			admux = ADMUX;
 			admux &= 0xf0;
 
 			if(sensor == 0)
-				admux |= 0x00; // 0x0 = ADC0 = TMP36
-			else
 				admux |= 0x08; // 0x8 = ADC8 = bg
+			else
+				admux |= 0x00; // 0x0 = ADC0 = TMP36
 
 			ADMUX = admux;
 
@@ -105,9 +105,9 @@ uint8_t application_function_temp_read(uint8_t nargs, uint8_t args[application_n
 			raw_v = ((float)raw / (float)samples) / 1000 * eeprom_read_bandgap();
 
 			if(sensor == 0)
-				temp = (raw_v - 0.5) * 100;	// TMP36
-			else
 				temp = (raw_v - 0.2897) * 0.942 * 1000; // bg
+			else
+				temp = (raw_v - 0.5) * 100;	// TMP36
 
 			snprintf_P((char *)dst, size, ok, sensor, temp, raw_v);
 

88c74ce7f9a13a2939f96177c5d133e1a208e116
Erik Slagter <erik@slagter.name>
2014-10-20 17:45:54 +0200

    Add audible ping.

--- icmp4.c
+++ icmp4.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "application.h"
 #include "ipv4.h"
 #include "icmp4.h"
 
@@ -8,10 +9,9 @@ uint16_t process_icmp4(uint16_t length, const uint8_t *message,
 		uint16_t reply_size, uint8_t *reply)
 		
 {
-	const	icmp4_echo_message_t *src = (icmp4_echo_message_t *)message;
-			icmp4_echo_message_t *dst = (icmp4_echo_message_t *)reply;
-
-	uint16_t current;
+	const				icmp4_echo_message_t *src = (icmp4_echo_message_t *)message;
+						icmp4_echo_message_t *dst = (icmp4_echo_message_t *)reply;
+	static const char 	*cmd = "beep 3 0";
 
 	if((src->type != ic4_echo_request) || (src->code != 0x00))
 		return(0);
@@ -19,13 +19,13 @@ uint16_t process_icmp4(uint16_t length, const uint8_t *message,
 	if(ipv4_checksum(length, message, 0, 0) != 0)
 		return(0);
 
-	for(current = 0; current < length; current++)
-		reply[current] = message[current];
+	application_content(strlen(cmd), (const uint8_t *)cmd, reply_size, (uint8_t *)reply);
+
+	memcpy(dst, src, length);
 
 	dst->type		= ic4_echo_reply;
 	dst->checksum	= 0;
-
-	dst->checksum = ipv4_checksum(length, reply, 0, 0);
+	dst->checksum	= ipv4_checksum(length, reply, 0, 0);
 
 	return(length);
 }

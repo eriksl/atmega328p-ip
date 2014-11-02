b5324fcab68a9cd3632fd433b4eb1d0e479d2eb1
Erik Slagter <erik@slagter.name>
2014-11-02 16:02:05 +0100

    Add tsl2560 light sensor as sensor 5/6.

--- application-sensor.c
+++ application-sensor.c
@@ -24,14 +24,64 @@ ISR(ADC_vect)
 	adc_interrupts++;
 }
 
+uint8_t tsl2560_write(uint8_t reg, uint8_t value)
+{
+	uint8_t twierror;
+	uint8_t twistring[2];
+
+	twistring[0] = 0xc0 | reg; // write byte
+	twistring[1] = value;
+
+	if((twierror = twi_master_send(0x39, 2, twistring)) != tme_ok)
+		return(twierror);
+
+	return(0);
+}
+
+uint8_t tsl2560_read(uint8_t reg, uint8_t *value)
+{
+	uint8_t twierror;
+	uint8_t twistring[1];
+
+	twistring[0] = 0xc0 | reg; // read byte
+
+	if((twierror = twi_master_send(0x39, 1, twistring)) != tme_ok)
+		return(twierror);
+
+	if((twierror = twi_master_receive(0x39 , 1, value)) != tme_ok)
+		return(twierror);
+
+	return(0);
+}
+
+uint8_t tsl2560_read_quad(uint8_t reg, uint8_t *values)
+{
+	uint8_t twierror;
+	uint8_t twistring[1];
+
+	twistring[0] = 0xd0 | reg; // read block
+
+	if((twierror = twi_master_send(0x39, 1, twistring)) != tme_ok)
+		return(twierror);
+
+	if((twierror = twi_master_receive(0x39 , 4, values)) != tme_ok)
+		return(twierror);
+
+	return(0);
+}
+
 void application_init_sensor(void)
 {
 	PRR			&= ~_BV(PRADC);
 	DIDR0		= _BV(ADC0D) | _BV(ADC1D);
 	ADMUX		= _BV(REFS1) | _BV(REFS0) | 0x0e;	// ref = 1.1V, input = 1.1V
 	ADCSRA		= _BV(ADEN)  | _BV(ADIF)  |
-				  _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);	// 8 Mhz / 128 = 75 kHz > 50 < 200 kHz
+				  _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);	// 11 Mhz / 128 = 85 kHz = > 50 < 200 kHz
 	ADCSRB		=	0x00;
+
+	tsl2560_write(0x00, 0x03);	// power up tsl2560
+	tsl2560_write(0x01, 0x01);	// set timing to 100 ms = 0b01
+	tsl2560_write(0x06, 0x00);	// disable interrupts
 }
 
 static uint16_t get_adc(void)
@@ -69,7 +119,6 @@ uint8_t application_sensor_read(uint8_t sensor, uint16_t size, uint8_t *dst)
 	static const __flash char format_humidity[]	= "> sensor %d ok humidity [%.0f] %%\n";
 	static const __flash char format_light[]	= "> sensor %d ok light [%.3f] Lux\n";
 	static const __flash char twi_error[]		= "> sensor %d twi error\n";
-	static const __flash char overflow[]		= "> sensor %d overflow\n";
 
 	const __flash char *format;
 
@@ -183,6 +232,65 @@ uint8_t application_sensor_read(uint8_t sensor, uint16_t size, uint8_t *dst)
 			value = (value * 100) / 32768;
 
 			format = format_humidity;
+
+			break;
+		}
+
+		case(5): // tsl2560 on twi 0x39, low gain
+		case(6): // tsl2560 on twi 0x39, high gain
+		{
+			float ch0, ch1;
+
+			if(sensor == 5)
+				twistring[0] = 0b00000001;	// low gain 1x, integration time = 100 ms, scale = 0.252
+			else
+				twistring[0] = 0b00010001;	// high gain 16, integration time = 100 ms, scale = 0.252 * 16
+
+			if((twierror = tsl2560_write(0x01, twistring[0])) != tme_ok)
+			{
+				snprintf_P((char *)dst, size, twi_error, sensor);
+				return(1);
+			}
+
+			if((twierror = tsl2560_read_quad(0x0c, twistring)) != tme_ok)
+			{
+				snprintf_P((char *)dst, size, twi_error, sensor);
+				return(1);
+			}
+
+
+			ch0 = twistring[0] | (twistring[1] << 8);
+			ch1 = twistring[2] | (twistring[3] << 8);
+
+			if(ch0 != 0)
+				raw_value = ch1 / ch0;
+			else
+				raw_value = 0;
+
+			if(raw_value <= 0.50)
+				value = (0.0304 * ch0) - (0.062 * ch0 * powf(raw_value, 1.4));
+			else if(raw_value <= 0.61)
+				value = (0.0224 * ch0) - (0.031 * ch1);
+			else if(raw_value <= 0.80)
+				value = (0.0128 * ch0) - (0.0153 * ch1);
+			else if(raw_value <= 1.30)
+				value = (0.00146 * ch0) - (0.00112 * ch1);
+			else
+				value = 0;
+
+			if(isnan(value))
+			{
+				value = 10000;
+			}
+			else
+			{
+				if(sensor == 6)
+					value /= 16;
+
+				value /= 0.252; // integration time = 100 ms, scale = 0.252
+			}
+
+			format = format_light;
 
 			break;
 		}

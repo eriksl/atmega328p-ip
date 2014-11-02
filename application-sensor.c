5c33130b88ec67a55e4074171f738f6594e29e43
Erik Slagter <erik@slagter.name>
2014-11-02 12:50:37 +0100

    Remove tsl2550 support, it's replaced by tsl2560 support which uses the same i2c address (0x39) so can't be used simultanuously.

--- application-sensor.c
+++ application-sensor.c
@@ -24,120 +24,6 @@ ISR(ADC_vect)
 	adc_interrupts++;
 }
 
-enum
-{
-	tsl_read_ok = 0,
-	tsl_read_io_error = 1,
-	tsl_read_overflow = 2,
-};
-
-static uint8_t tsl_adc2count(uint8_t in, uint16_t *out)
-{
-	uint8_t	valid	= !!(in & 0x80);
-	uint8_t	chord	= (in & 0x70) >> 4;
-	uint8_t	step	= (in & 0x0f);
-
-	if(!valid)
-		return(tsl_read_io_error);
-
-	if((in & 0x7f) == 0x7f)
-		return(tsl_read_overflow);
-
-	uint16_t	chordval	= (33 * ((1 << chord) - 1)) / 2;
-	uint8_t		stepval		= step * (1 << chord);
-
-	*out = chordval + stepval;
-
-	return(tsl_read_ok);
-}
-
-static float tsl_count2lux(uint16_t ch0, uint16_t ch1, uint8_t multiplier)
-{
-	float r, e, l;
-
-	if(ch0 == ch1)
-		r = 0;
-	else
-		r = (float)ch1 / ((float)ch0 - (float)ch1);
-
-	e = exp(-0.181 * r * r);
-	l = ((float)ch0 - (float)ch1) * 0.39 * e * (float)multiplier;
-
-	if(l > 100)
-		l = round(l);
-	else if(l > 10)
-		l = round(l * 10) / 10;
-	else
-		l = round(l * 100) / 100;
-
-	return(l);
-}
-
-static uint8_t tsl_write_read(uint8_t *byte)
-{
-	uint8_t twierror;
-
-	if((twierror = twi_master_send(0x39, 1, byte)) != tme_ok)
-		return(0);
-
-	if((twierror = twi_master_receive(0x39, 1, byte)) != tme_ok)
-		return(0);
-
-	return(1);
-}
-
-static uint8_t tsl_read_sensor(float *result, uint8_t extended_range)
-{
-	uint8_t		byte;
-	uint8_t		rv;
-	uint8_t		ch0, ch1;
-	uint16_t	cch0, cch1;
-
-	byte = 0x03;	// power up (if not already running)
-
-	if(!tsl_write_read(&byte))
-		return(tsl_read_io_error);
-
-	if(byte != 0x03)
-		return(tsl_read_io_error);
-
-	if(extended_range)
-		byte = 0x1d;	// extended range
-	else
-		byte = 0x18;	// standard range
-
-	if(!tsl_write_read(&byte))
-		return(tsl_read_io_error);
-
-	if(byte != 0x1b)
-		return(tsl_read_io_error);
-
-	byte = 0x43;	// read from channel 0
-
-	if(!tsl_write_read(&byte))
-		return(tsl_read_io_error);
-
-	ch0 = byte;
-
-	byte = 0x83;	// read from channel 1
-
-	if(!tsl_write_read(&byte))
-		return(tsl_read_io_error);
-
-	ch1 = byte;
-
-	if((rv = tsl_adc2count(ch0, &cch0)) != tsl_read_ok)
-		return(rv);
-
-	if((rv = tsl_adc2count(ch1, &cch1)) != tsl_read_ok)
-		return(rv);
-
-	if((*result = tsl_count2lux(cch0, cch1, extended_range ? 5 : 1)) < 0)
-		return(tsl_read_io_error);
-
-	return(tsl_read_ok);
-}
-
 void application_init_sensor(void)
 {
 	PRR			&= ~_BV(PRADC);
@@ -146,9 +32,6 @@ void application_init_sensor(void)
 	ADCSRA		= _BV(ADEN)  | _BV(ADIF)  |
 				  _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);	// 8 Mhz / 128 = 75 kHz > 50 < 200 kHz
 	ADCSRB		=	0x00;
-
-	uint8_t byte = 0x03; // power up tsl2550
-	tsl_write_read(&byte);
 }
 
 static uint16_t get_adc(void)
@@ -300,29 +183,6 @@ uint8_t application_sensor_read(uint8_t sensor, uint16_t size, uint8_t *dst)
 			value = (value * 100) / 32768;
 
 			format = format_humidity;
-
-			break;
-		}
-
-		case(5): // tsl2550 standard range on twi
-		case(6): // tsl2550 extended range on twi
-		{
-			switch(tsl_read_sensor(&value, (sensor == 6)))
-			{
-				case(tsl_read_io_error):
-				{
-					snprintf_P((char *)dst, size, twi_error, sensor);
-					return(1);
-				}
-
-				case(tsl_read_overflow):
-				{
-					snprintf_P((char *)dst, size, overflow, sensor);
-					return(1);
-				}
-			}
-
-			format = format_light;
 
 			break;
 		}

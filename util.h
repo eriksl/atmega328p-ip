e52c1f03971efe874381a25b326fd39e7cbb22fe
Erik Slagter <erik@slagter.name>
2015-03-29 19:15:57 +0200

    Add generic crc8 calculating function.

--- util.h
+++ util.h
@@ -3,8 +3,9 @@
 
 #include <stdint.h>
 
-void reset(void);
-void msleep(uint16_t ms);
-void pause_idle(void);
+void	reset(void);
+void	msleep(uint16_t ms);
+void	pause_idle(void);
+uint8_t crc8(uint8_t crc_mode, uint8_t length, const uint8_t *block);
 
 #endif

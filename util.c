62af72971e98a0e2d56d7f0dde945a66a3f231cd
Erik Slagter <erik@slagter.name>
2015-03-29 19:15:57 +0200

    Add generic crc8 calculating function.

--- util.c
+++ util.c
@@ -23,3 +23,26 @@ void pause_idle(void)
 	set_sleep_mode(SLEEP_MODE_IDLE);
 	sleep_mode();
 }
+
+uint8_t crc8(uint8_t crc_mode, uint8_t length, const uint8_t *block)
+{
+	uint8_t outer, inner, crc = 0;
+
+	for(outer = 0; outer < length; outer++)
+	{
+		crc = crc ^ block[outer];
+
+		for(inner = 0; inner < 8; inner++)
+		{
+			if((crc & 0x80) != 0)
+			{
+				crc <<= 1;
+				crc ^= crc_mode;
+			}
+			else
+				crc <<= 1;
+		}
+	}
+
+	return(crc);
+}

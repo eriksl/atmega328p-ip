874f4de01a60680546d4f0efae664a65d68357b7
Erik Slagter <erik@slagter.name>
2015-04-02 16:50:38 +0200

    Show activity led for any activity, even when spurious.

--- application.c
+++ application.c
@@ -200,6 +200,9 @@ int16_t application_content(uint16_t src_length, const uint8_t *src, uint16_t si
 	uint8_t ws_skipped;
 	const application_function_table_t __flash *tableptr;
 
+	cmd_led_timeout = 10;
+	PORTD |= _BV(3);
+
 	if((src_length == 0) || (src[0] == 0xff)) // telnet options
 		return(0);
 
@@ -239,9 +242,6 @@ int16_t application_content(uint16_t src_length, const uint8_t *src, uint16_t si
 	}
 
 	*dst = '\0';
-
-	cmd_led_timeout = 10;
-	PORTD |= _BV(3);
 
 	if(args_count == 0)
 		return(0);
